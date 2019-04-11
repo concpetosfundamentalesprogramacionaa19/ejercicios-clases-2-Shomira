9e86aab72ba54d027ae4336f8f606871ea4cc434
Shomira <shomirarosales01@gmail.com>
2019-04-11 10:40:11 -0500

    proyecto java

---
+++ Ejemplosclase2/src/ejemplosclase2/Ejemplosclase2.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Salas
+ */
+public class Ejemplosclase2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Este metodo es el principal de mi proyecto
+        // No olvidar declarar variables
+        
+        String nombre;
+        String apellido;
+        Scanner entrada=new Scanner(System.in);
+        
+        System.out.println("Porfavor ingresar su nombre: ");
+        nombre = entrada.next();
+        System.out.println("Porfavor ingrese su apellido: ");
+        apellido = entrada.next();
+        System.out.println("Su nombre es : "+nombre+"\n\n\t"+"Su apellido es : "
+                +apellido );
+
+    }
+    
+}
